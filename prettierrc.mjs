000c0b2c56d8f0c6f2592e28c0dce3c314292c78
lofi <jsj2505@gmail.com>
2025-07-12 17:26:49 +0900

    initial commit

---
+++ prettierrc.mjs
@@ -0,0 +1,13 @@
+const config = {
+  trailingComma: "es5",
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  printWidth: 140,
+  arrowParens: "always",
+  endOfLine: "lf",
+  jsxSingleQuote: true,
+  bracketSpacing: true,
+};
+
+export default config;
