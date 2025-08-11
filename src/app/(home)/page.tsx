ff2b0ea9c1f9d968922b6ee673397019b7bd4699
lofi <jsj2505@gmail.com>
2025-08-11 10:48:55 +0900

    [code] log 제거

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,12 +1,10 @@
 import { IntroduceSection } from "@/features/home/components/introduce-section";
 import { RecomendedPostSection } from "@/features/home/components/recomended-post-section";
 import { TechStackSection } from "@/features/home/components/tech-stack-section";
-import { healthCheck } from "@/shared/lib/utils";
 
 export default async function Home() {
-  const res = await healthCheck();
-  console.log("---------------------");
-  console.log(JSON.stringify(res, null, 2));
+  // const res = await healthCheck();
+
   return (
     <div className="flex flex-col gap-10 items-center">
       <IntroduceSection />
