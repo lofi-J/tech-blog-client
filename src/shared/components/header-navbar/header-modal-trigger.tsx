e0fea822da71c571134c84d4d78dc72b48f19dc8
lofi <jsj2505@gmail.com>
2025-08-18 14:29:10 +0900

    [wip] search modal

--- src/shared/components/header-navbar/header-modal-trigger.tsx
+++ src/shared/components/header-navbar/header-modal-trigger.tsx
@@ -18,7 +18,7 @@ export const HeaderModalTrigger = () => {
       e.stopPropagation();
       openModal();
     },
-    [openModal],
+    [openModal]
   );
 
   useEffect(() => {
@@ -40,7 +40,7 @@ export const HeaderModalTrigger = () => {
       <Button
         variant="ghost"
         onClick={onClick}
-        className="px-2 min-w-[200px] bg-secondary flex items-center justify-between hover:bg-secondary/80"
+        className="px-4 min-w-[200px] bg-secondary flex items-center justify-between hover:bg-secondary/80"
       >
         <span className="text-muted-foreground text-[13px] font-light">
           Search any keywords...
@@ -65,7 +65,7 @@ const Kbd = ({
     <kbd
       className={cn(
         "text-foreground bg-background border border-border rounded-[4px] px-1 text-[11px] text-center w-5 h-5 flex items-center justify-center",
-        className,
+        className
       )}
     >
       {children}
--- src/shared/components/search-modal.tsx
+++ src/shared/components/search-modal.tsx
@@ -1,17 +1,89 @@
+"use client";
+
+import { useCallback, useEffect, useState } from "react";
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "./ui/dialog";
+import { Input } from "./ui/input";
+
+type SearchResultType = "post" | "skill" | "setting";
+
+// TODO : action callback?
+type SearchResult = {
+  type: SearchResultType;
+  icon: React.ReactNode;
+  title: string;
+};
 
 type SearchModalProps = {
   isOpen: boolean;
   setIsOpen: (isOpen: boolean) => void;
 };
+
 export const SearchModal = ({ isOpen, setIsOpen }: SearchModalProps) => {
+  const [keyword, setKeyword] = useState("");
+  const [searchResult, setSearchResult] = useState<SearchResult[]>([]);
+
+  const closeModal = () => setIsOpen(false);
+
+  const isEmptySearchResult = searchResult.length === 0;
+
+  // Input onChange Fn
+  const handleSearchChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setKeyword(e.target.value);
+    },
+    []
+  );
+
+  // search from index TODO : search and sosrting [post, skill, setting]
+  useEffect(() => {
+    setSearchResult([]);
+  }, [keyword]);
+
   return (
     <Dialog open={isOpen} onOpenChange={setIsOpen}>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Search</DialogTitle>
+      <DialogContent
+        showCloseButton={false}
+        disableDescription={true}
+        className="p-0 gap-0"
+      >
+        <DialogHeader className="p-2">
+          <DialogTitle>
+            <Input
+              id="search-input"
+              placeholder="Search any keywords..."
+              type="search"
+              className="w-full placeholder:text-[12px]"
+              value={keyword}
+              onChange={handleSearchChange}
+            />
+          </DialogTitle>
         </DialogHeader>
+        <div className="p-2">
+          {isEmptySearchResult && <EmptySearchResult />}
+          {!isEmptySearchResult &&
+            searchResult.map((result) => (
+              <div
+                key={`${result.type}-${result.title}`}
+                className="bg-red-400"
+                onClick={() => {
+                  console.log(result);
+                  closeModal();
+                }}
+              >
+                {result.title}
+              </div>
+            ))}
+        </div>
       </DialogContent>
     </Dialog>
+  );
+};
+
+// TODO: empty state content example: setting, recommend, etc...
+const EmptySearchResult = () => {
+  return (
+    <div className="flex justify-center items-center h-full">
+      <div className="text-muted-foreground text-sm">No results found</div>
+    </div>
   );
 };
