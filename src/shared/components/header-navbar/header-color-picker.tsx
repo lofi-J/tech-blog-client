9dc11811ee43624bba09249ae3e449748471ae73
lofi <jsj2505@gmail.com>
2025-08-28 12:34:56 +0900

    [feat] color picker 추천색상, input

--- src/shared/components/header-navbar/header-color-picker.tsx
+++ src/shared/components/header-navbar/header-color-picker.tsx
@@ -1,10 +1,31 @@
 "use client";
 
 import { useColor } from "@/shared/context/color-provider";
+import { Theme } from "@/shared/context/theme-provider";
 import { cn } from "@/shared/lib/utils";
 import { atom, useAtom } from "jotai";
+import { useTheme } from "next-themes";
 import { useEffect, useRef } from "react";
 import * as ColorPicker from "react-colorful";
+
+export const COLOR_SET = {
+  dark: {
+    red: "#ca232b",
+    orange: "#f25116",
+    yellow: "#7ccf00",
+    green: "#2fe564",
+    blue: "#2b7fff",
+    purple: "#ad46ff",
+  },
+  light: {
+    red: "#ca232b",
+    orange: "#ff3e00",
+    yellow: "#7fab00",
+    green: "#2fe564",
+    blue: "#2079ff",
+    purple: "#8200ff",
+  },
+};
 
 // Jota global variable
 export const openColorPicker = atom(false);
@@ -13,6 +34,17 @@ export const HeaderColorPicker = ({ className }: { className?: string }) => {
   const containerRef = useRef<HTMLDivElement>(null);
   const [open, setOpen] = useAtom(openColorPicker);
   const { highlightColor, setHighlightColor } = useColor();
+  const { theme } = useTheme();
+
+  const getDefaultTheme = (theme?: string): Theme => {
+    if (!theme) {
+      return "dark";
+    }
+    if (theme === "dark") {
+      return "dark";
+    }
+    return "light";
+  };
 
   const handleColorChange = (newColor: string) => {
     setHighlightColor(newColor);
@@ -58,12 +90,77 @@ export const HeaderColorPicker = ({ className }: { className?: string }) => {
       </div>
       {open && (
         <div className="absolute top-10 right-0 z-50 bg-background border border-border rounded-lg p-3 shadow-lg">
-          <ColorPicker.HexAlphaColorPicker
-            color={highlightColor}
-            onChange={handleColorChange}
-          />
+          <div className="f-col gap-1">
+            <ColorPicker.HexAlphaColorPicker
+              color={highlightColor}
+              onChange={handleColorChange}
+            />
+            <div className="f-col gap-1">
+              <div className="text-[12px] font-semibold text-highlight">
+                Recommended color
+              </div>
+              <div className="flex gap-1.5 items-center justify-around">
+                <ColorCircle
+                  color={COLOR_SET[getDefaultTheme(theme)].red}
+                  setHighlightColor={handleColorChange}
+                />
+                <ColorCircle
+                  color={COLOR_SET[getDefaultTheme(theme)].orange}
+                  setHighlightColor={handleColorChange}
+                />
+                <ColorCircle
+                  color={COLOR_SET[getDefaultTheme(theme)].yellow}
+                  setHighlightColor={handleColorChange}
+                />
+                <ColorCircle
+                  color={COLOR_SET[getDefaultTheme(theme)].green}
+                  setHighlightColor={handleColorChange}
+                />
+                <ColorCircle
+                  color={COLOR_SET[getDefaultTheme(theme)].blue}
+                  setHighlightColor={handleColorChange}
+                />
+                <ColorCircle
+                  color={COLOR_SET[getDefaultTheme(theme)].purple}
+                  setHighlightColor={handleColorChange}
+                />
+              </div>
+              <div className="mt-2">
+                <ColorPicker.HexColorInput
+                  color={highlightColor}
+                  onChange={handleColorChange}
+                  className="w-full rounded-sm bg-input/60 border border-secondary px-2 focus-visible:outline-none"
+                />
+              </div>
+            </div>
+          </div>
         </div>
       )}
     </div>
+  );
+};
+
+const ColorCircle = ({
+  color,
+  setHighlightColor,
+  className,
+}: {
+  color: string;
+  setHighlightColor: (color: string) => void;
+  className?: string;
+}) => {
+  const { theme } = useTheme();
+  const isDark = theme === "dark";
+  return (
+    <button
+      onClick={() => setHighlightColor(color)}
+      className={cn(
+        "hover:scale-110 transition-all duration-300 cursor-pointer w-[25px] h-[25px] rounded-full border",
+        className,
+        isDark ? "border-white" : "border-black"
+      )}
+      style={{ backgroundColor: color }}
+      aria-label={color}
+    />
   );
 };
