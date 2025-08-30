443cb3cccc7d29d37b3116957efd3bf76dced71b
lofi <jsj2505@gmail.com>
2025-08-30 16:26:01 +0900

    [fix] 불필요한 코드 제거

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -66,52 +66,6 @@ const components: MDXComponents = {
   },
 };
 
-const previewComponents: MDXComponents = {
-  h1: ({ children }) => <h1 className="rts-16 font-bold mb-4">{children}</h1>,
-  h2: ({ children }) => (
-    <h2 className="rts-15 font-semibold mb-3">{children}</h2>
-  ),
-  h3: ({ children }) => (
-    <h3 className="rts-14 font-semibold mb-3 text-gray-800">{children}</h3>
-  ),
-  p: ({ children }) => (
-    <p className="mb-2 rts-14 leading-5 line-clamp-3">{children}</p>
-  ),
-  ul: ({ children }) => (
-    <ul className="mb-4 list-disc list-inside space-y-2 rts-14 truncate">
-      {children}
-    </ul>
-  ),
-  li: ({ children }) => (
-    <li className="leading-4 rts-12 truncate">{children}</li>
-  ),
-  strong: ({ children }) => (
-    <strong className="font-semibold rts-12 leading-4">{children}</strong>
-  ),
-  code: ({ children }) => (
-    <code className="px-1 py-0.5 bg-gray-100 rounded text-sm font-mono text-pink-600 rts-12">
-      {children}
-    </code>
-  ),
-  pre: ({ children }) => (
-    <pre className="mb-4 p-2 bg-gray-900 rounded-lg overflow-x-auto rts-12">
-      {children}
-    </pre>
-  ),
-  del: ({ children }) => (
-    <del className="line-through text-gray-500 rts-12">{children}</del>
-  ),
-  Image: (props: ImageProps) => {
-    return (
-      <Image
-        {...props}
-        alt={props.alt ?? "unknown image"}
-        className="w-full h-auto"
-      />
-    );
-  },
-};
-
 // 서버 컴포넌트에서 사용할 함수
 export function getMDXComponents(
   otherComponents?: MDXComponents
@@ -122,24 +76,9 @@ export function getMDXComponents(
   };
 }
 
-export function getPreviewMDXComponents(
-  otherComponents?: MDXComponents
-): MDXComponents {
-  return {
-    ...previewComponents,
-    ...otherComponents,
-  };
-}
-
 // 클라이언트 컴포넌트에서 사용하는 Hook
 export function useMDXComponents(
   otherComponents?: MDXComponents
 ): MDXComponents {
   return getMDXComponents(otherComponents);
-}
-
-export function usePreviewMDXComponents(
-  otherComponents?: MDXComponents
-): MDXComponents {
-  return getPreviewMDXComponents(otherComponents);
 }
