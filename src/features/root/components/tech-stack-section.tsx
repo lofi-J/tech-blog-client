056608ebc58bec26a8b144f3929fe09e2090caaa
lofi <jsj2505@gmail.com>
2025-08-31 16:53:55 +0900

    [add] query CategoryUsageStatsDocument

--- src/features/root/components/tech-stack-section.tsx
+++ src/features/root/components/tech-stack-section.tsx
@@ -1,3 +1,12 @@
-export const TechStackSection = () => {
+import client from "@/client";
+import { CategoryUsageStatsDocument } from "@/generated/graphql";
+
+export const TechStackSection = async () => {
+  const { data } = await client.query({
+    query: CategoryUsageStatsDocument,
+  });
+
+  // console.log(JSON.stringify(data, null, 2));
+  
   return <div>TechStackSection</div>;
 };
